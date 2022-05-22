6137ca31561615d91087b0e7a7aa0f540ff0ac27
Kristy Guo <krisxxg@gmail.com>
2022-05-21 17:46:41 -0700

    add seed.js in utils folder

--- utils/seed.js
+++ utils/seed.js
@@ -1,3 +1,42 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
-const { getRandomName, getRandomThoughts } = require('./data');
+const {getRandomName, getRandomThoughts, getThoughtReaction} = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  await Thought.deleteMany({});
+  await User.deleteMany({});
+
+  const users = [];
+  const thoughts = getRandomThoughts(9);
+
+
+  for (let i = 0; i < 9; i++) {
+    const username = getRandomName();
+    const email = getRandomEmail();
+    users.push({
+     username,
+     email, 
+    });
+  }
+
+  function getRandomEmail() {
+    var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    var string = '';
+    for (var i=0; i<15; i++) {
+        string += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return (string + '@gmail.com');
+  }
+
+
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
+
+  console.table(users);
+  console.table(thoughts);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
